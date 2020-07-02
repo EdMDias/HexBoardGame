9e972270e65f6c48f158aafdc30142428bc32ace
EdMDias <eduardo.m.dias@gmail.com>
2020-07-02 13:03:40 +0100

    v2 2020/7/2 first version of Board finished

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import numpy as np
 
-class board():
+class Board():
+
+    player1 = 1
+    player2 = -1
+    # player1 plays left to right and player2 plays top to bottom
+
     def __init__(self,size):
         self.board = np.zeros((3,size,size))
         self.size = size
@@ -13,28 +18,35 @@ class board():
 
     def neighborhood(self, x, y):
         for i in hex_directions:
-            if i[0]+x >= 0 and i[0]+x < self.size and i[1]+y >= 0 and i[1]+y < self.size:
-                yield i
+            if 0 <= i[0]+x < self.size and 0 <= i[1] + y < self.size:
+                yield [i[0] + x, i[1] + y]
 
     def paint_neighbor(self, x, y, level):
         player = self.board[0, x, y]
-        for i in self.neighborhood(x,y):
-            if self.board[0,i[0],i[1]] == player and  self.board[level,i[0],i[1]] == 0:
+        for i in self.neighborhood(x, y):
+            if self.board[0, i[0], i[1]] == player and self.board[level, i[0], i[1]] == 0:
                 self.board[level, i[0], i[1]] = 1
                 self.paint_neighbor(i[0], i[1], level)
 
-
     def check_conn_start(self, x, y, player):
-        if (x==0 and player == player1) or (y==0 and player==player2):
-            board[1,x,y]=1
-            # set board[1,a,b]=1 for neighbors of [a,b] that have the same piece
-        #elif one neighbor of [x,y] has [1,a,b]=1
-            # board[1,x,y]=1
-            # set board[1,a,b]=1 for neighbors of [a,b] that have the same piece
+        if (x == 0 and player == self.player1) or (y == 0 and player == self.player2):
+            self.board[1, x, y] = 1
+            self.paint_neighbor(x, y, 1)
+        else:
+            for i in self.neighborhood(x, y):
+                if self.board[0, i[0], i[1]] == player and self.board[1, i[0], i[1]] == 1:
+                    self.board[0, x, y] = 1
+                    self.paint_neighbor(x, y, 1)
 
     def check_conn_end(self,x,y):
         if (x == self.size and player == player1) or (y == self.size and player == player2):
-            board[2, x, y] = 1
+            self.board[2, x, y] = 1
+            self.paint_neighbor(x, y, 2)
+        else:
+            for i in self.neighborhood(x, y):
+                if self.board[0, i[0], i[1]] == player and self.board[1, i[0], i[1]] == 2:
+                    self.board[0, x, y] = 1
+                    self.paint_neighbor(x, y, 2)
 
-
-
+    def check_victory(self, x, y):
+        return self.board[1, x, y] == self.board[2, x, y] == 1
