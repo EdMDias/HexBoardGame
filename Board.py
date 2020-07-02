35196fa63d1a98959d76922d81b5d16907d5fadb
EdMDias <eduardo.m.dias@gmail.com>
2020-07-02 16:45:13 +0100

    v7 2020/7/2 first playable version

--- Board.py
+++ Board.py
@@ -4,15 +4,14 @@ class Board():
 
     player1 = 1
     player2 = -1
-    # player1 plays top down (x == 0 to x == self.size - 1) and player2 plays left right (y == 0 to y == self.size -1 )
+    # player1 plays left right (y == 0 to y == self.size - 1) and player2 plays top down (x == 0 to x == self.size -1 )
 
     def __init__(self,size):
         self.board = np.zeros((2,size,size))
         self.size = size
 
     def place_piece(self,x,y,player):
-        if self.board[0,x,y]==0:
-            self.board[0,x,y] = player
+        self.board[0,x,y] = player
 
     hex_directions = [[-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0]]
 
@@ -29,17 +28,20 @@ class Board():
                 self.paint_neighbor(i[0], i[1])
 
     def check_conn_start(self, x, y, player):
-        if (x == 0 and player == self.player1) or (y == 0 and player == self.player2):
+        if (x == 0 and player == self.player2) or (y == 0 and player == self.player1):
             self.board[1, x, y] = 1
             self.paint_neighbor(x, y)
         else:
             for i in self.neighborhood(x, y):
                 if self.board[0, i[0], i[1]] == player and self.board[1, i[0], i[1]] == 1:
-                    self.board[0, x, y] = 1
+                    self.board[1, x, y] = 1
                     self.paint_neighbor(x, y)
 
-    def check_victory(self):
-        return sum(self.board[1, :, self.size - 1]) == 1 or sum(self.board[1, self.size - 1, :]) == 1
+    def check_victory(self,player):
+        if player == self.player1:
+            return sum(self.board[1, :, self.size - 1]) > 0
+        else:
+            return sum(self.board[1, self.size - 1, :]) > 0
 
     def print_board(self):
         for i in range(self.size):
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import Board
+import numpy as np
 
+print('The player 1 plays from left to right and the player -1 plays top down!\n Have fun.')
 board = Board.Board(int(input('Choose the size of the board\n')))
+board.print_board()
 
 def turn(board,player):
-    position_str = input('Choose a position to play - tow integers separated by a comma\n')
-    x , y = int(position_str.split(',')[0]),int(position_str.split(',')[1])
-    board.place_piece(x, y, player)
-    board.check_conn_start(x, y, player)
+    while True:
+        position_str = input(f'Player {player}:\nChoose a position to play as two integers (between 0 and {board.size - 1}) separated by a comma\n')
+        x , y = int(position_str.split(',')[0]),int(position_str.split(',')[1])
+        if board.board[0, x, y] == 0:
+            board.place_piece(x, y, player)
+            board.check_conn_start(x, y, player)
+            break
 
-player_on = -1
+player_on = (-1)**np.random.randint(0,2)
 while True:
     player_on *= -1
     turn(board,player_on)
-    if board.check_victory():
+    if board.check_victory(player_on):
         board.print_board()
         print(f'Player {player_on} won!')
         break
